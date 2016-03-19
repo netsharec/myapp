44d97ed81c7533e344b01d232eb93f1f9f59cf48
mingang.he <netsharec@gmail.com>
2016-03-19 16:51:51 +0800

    Add chi

--- hello.go
+++ hello.go
@@ -3,12 +3,24 @@ package hello
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/net/context"
+
+	"github.com/pressly/chi"
 )
 
 func init() {
-	http.HandleFunc("/", handler)
+	var router = chi.NewRouter()
+	router.Get("/", handler)
+	router.Get("/:id", getId)
+	http.Handle("/", router)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, world!")
+}
+
+func getId(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParams(ctx)["id"]
+	fmt.Fprintf(w, "id: %v", id)
 }
